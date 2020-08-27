5b50a78f4ec9b85ee25f89cdecf7129b6f5da713
Desmond <desmondaldridge@gmail.com>
2020-08-27 15:26:27 -0700

    Seems like it should be so simple, but I cannot figure out how to have the saved search buttons repopulate the weather data for that city...

--- assets/buttons.js
+++ assets/buttons.js
@@ -1,38 +1,61 @@
 
-      var pastSearches = [];
+var pastSearches = [];
 
-      // Displaying past searches
-      function renderButtons() {
+// Displaying past searches
+function renderButtons() {
 
-        $("#searches").empty();
+    $("#searches").empty();
 
 
-        for (var i = 0; i < pastSearches.length; i++) {
+    for (var i = 0; i < pastSearches.length; i++) {
 
-          var a = $("<button>");
+        var a = $("<button>");
 
-          a.addClass("searched-city");
+        a.addClass("searched-city");
 
-          a.attr("data-name", pastSearches[i]);
+        a.attr("data-name", pastSearches[i]);
 
-          a.text(pastSearches[i]);
+        a.text(pastSearches[i]);
 
-          $("#searches").append(a);
-        }
-      }
+        $("#searches").append(a);
+    }
+}
 
-      // Populating the previous searches list
-      $("#select-city").click(function(event) {
-        event.preventDefault();
+// Populating the previous searches list
+$("#select-city").click(function(event) {
+event.preventDefault();
 
-        var searchedCity = $("#city-input").val().trim().toUpperCase();
-        // I needed help with the syntax here, I originally (and mistakenly) tried this: searchedCity !== pastSearches.indexOf(searchedCity)
-        // Also tried to add an additional qualifier in the conditional to screen for searches that are !== to 404 errors (not actual cities)
-        if (pastSearches.indexOf(searchedCity) === -1) {
-        pastSearches.push(searchedCity);
-        }
+    var searchedCity = $("#city-input").val().trim().toUpperCase();
+    // I needed help with the syntax here, I originally (and mistakenly) tried this: searchedCity !== pastSearches.indexOf(searchedCity)
+    // Also tried to add an additional qualifier in the conditional to screen for searches that are !== to 404 errors (not actual cities)
+    if (pastSearches.indexOf(searchedCity) === -1) {
+    pastSearches.push(searchedCity);
+    }
 
-        renderButtons();
-        
-      });
+    renderButtons();
+    
+});
+
+
+// var goToCity = searchWeather("data-name");
+
+// $(document).click(".searched-city", function() {
+//     var goToCity = $(this);
+//     searchWeather(goToCity);
+// });
+
+// // Calling the renderButtons function to display the initial buttons
+// renderButtons();
+
+
+
+// $(".searched-city").click(goToCity);
+
+// $(".searched-city").click(function(event) {
+//     event.preventDefault();
+
+//     var goToCity = $(this);
+
+//     searchWeather(goToCity);
+// })
 

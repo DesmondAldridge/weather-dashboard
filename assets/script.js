fc05f184ac741354c338a605fabe57affac75570
Desmond <desmondaldridge@gmail.com>
2020-08-27 19:57:53 -0700

    Consolidated the JS onto one page

--- assets/script.js
+++ assets/script.js
@@ -154,4 +154,49 @@ $("#select-city").click(function(event) {
   searchWeather(inputCity);
 });
 
-          
+var pastSearches = [];
+
+// Displaying past searches
+function renderButtons() {
+
+    $("#searches").empty();
+
+
+    for (var i = 0; i < pastSearches.length; i++) {
+
+        var a = $("<button>");
+
+        a.addClass("searched-city");
+
+        a.attr("data-name", pastSearches[i]);
+
+        a.text(pastSearches[i]);
+
+        $("#searches").append(a);
+    }
+}
+
+// Populating the previous searches list
+$("#select-city").click(function(event) {
+event.preventDefault();
+
+    var searchedCity = $("#city-input").val().trim().toUpperCase();
+    // I needed help with the syntax here, I originally (and mistakenly) tried this: searchedCity !== pastSearches.indexOf(searchedCity)
+    // Also tried to add an additional qualifier in the conditional to screen for searches that are !== to 404 errors (not actual cities)
+    if (pastSearches.indexOf(searchedCity) === -1) {
+    pastSearches.push(searchedCity);
+    }
+
+    renderButtons();
+    
+});
+
+$(".searched-city").click(function() {
+event.preventDefault();
+
+    var goToCity = $(this);
+
+    console.log(goToCity);
+    searchWeather(goToCity);
+});
+        
